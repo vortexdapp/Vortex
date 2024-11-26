fde848ef4940ab5d3248063d3c63fd34fb2ef7b2
Vortex Dapp <duarte.baltazar@vortexdapp.com>
2024-11-26 13:43:32 +0000

    a

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Ensures all paths are relative
+  build: {
+    outDir: 'dist', // Output directory
+  },
   plugins: [react()],
-  base: '/',
-})
+});
